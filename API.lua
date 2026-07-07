0f0c40a108b12bb45725469b5c78a4d6225b91dc
TheFortniteFreak <126966852+TheFortniteFreak@users.noreply.github.com>
2026-07-08 01:01:47 +0200

    Create API.lua

---
+++ API.lua
@@ -0,0 +1,39 @@
+local HttpService = game:GetService("HttpService")
+
+local ChatClient = {}
+
+ChatClient.MessageReceived = Instance.new("BindableEvent")
+
+local Socket
+
+function ChatClient.Connect(url)
+	Socket = WebSocket.connect(url)
+
+	Socket.OnMessage:Connect(function(message)
+		local data = HttpService:JSONDecode(message)
+
+		ChatClient.MessageReceived:Fire(
+			data.header,
+			data.content,
+			data.time
+		)
+	end)
+end
+
+
+function ChatClient.Send(header, content)
+	if not Socket then
+		warn("Not connected")
+		return
+	end
+
+	local packet = {
+		header = header,
+		content = content
+	}
+
+	Socket:Send(HttpService:JSONEncode(packet))
+end
+
+
+return ChatClient
